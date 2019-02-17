2265acdf6ca3573bd3ec59176a5ba222400c1f62
Andrei Zhidenko <zhidenko_1999@mail.ru>
2019-02-17 14:48:41 +0300

    implementation of IsNull method

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,12 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
-            throw new NotImplementedException();
+            bool result = false;
+
+            if (variable == null)
+                result = true;
+
+            return result;
         }
     }
 }
